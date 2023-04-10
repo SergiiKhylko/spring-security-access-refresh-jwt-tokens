d406829b3441140d336c39a00332e8c594c656bb
Sergii <sergii.khylko@outlook.com>
2023-04-10 13:20:01 +0300

    #0 init project

---
+++ src/main/java/com/ajkko/userservice/SpringSecurityAccessRefreshJwtTokensApplication.java
@@ -0,0 +1,13 @@
+package com.ajkko.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAccessRefreshJwtTokensApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityAccessRefreshJwtTokensApplication.class, args);
+    }
+
+}
